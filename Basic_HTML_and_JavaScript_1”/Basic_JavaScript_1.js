74d33c30f4cfb5ff0bb888b4db17a16acd14ad69
Noorahmad Noor <noornoorahmad694@gmail.com>
2022-04-09 12:09:33 -0700

    Assignment 2

---
+++ Basic_HTML_and_JavaScript_1”/Basic_JavaScript_1.js
@@ -0,0 +1,6 @@
+alert ("Hello, World! I am an alert box!");
+
+function My_First_Function(){
+	var string = "Kiss me, I am Irish!";
+	var result = string.fontcolor("green");
+	document.getElementById("Irish").innerHTML=result;}
